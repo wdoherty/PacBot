50dd8894155a3a44e638cf2c4b6e7e19592ae939
wdoherty <billydoherty5104@gmail.com>
2019-11-10 16:48:16 -0600

    added attach interrupts

--- PacBot2020/src/main.cpp
+++ PacBot2020/src/main.cpp
@@ -5,6 +5,11 @@
 
 Adafruit_PWMServoDriver pwm;
 
+encoder* enc1;
+encoder* enc2;
+encoder* enc3;
+encoder* enc4;
+
 void setup() {
   pwm = Adafruit_PWMServoDriver(0x48);
   Serial.begin(9600);
@@ -19,7 +24,19 @@ void setup() {
   pinMode(ENC4A, INPUT);
   pinMode(ENC4B, INPUT);
 
+  enc1 = new encoder(ENC1A, ENC1B);
+  enc2 = new encoder(ENC2A, ENC2B);
+  enc3 = new encoder(ENC3A, ENC3B);
+  enc4 = new encoder(ENC4A, ENC4B);
 
+  attachInterrupt(ENC1A, enc1->pin_change_a, CHANGE);
+  attachInterrupt(ENC1B, enc1->pin_change_b, CHANGE);
+  attachInterrupt(ENC2A, enc2->pin_change_a, CHANGE);
+  attachInterrupt(ENC2B, enc2->pin_change_b, CHANGE);
+  attachInterrupt(ENC3A, enc3->pin_change_a, CHANGE);
+  attachInterrupt(ENC3B, enc3->pin_change_b, CHANGE);
+  attachInterrupt(ENC4A, enc4->pin_change_a, CHANGE);
+  attachInterrupt(ENC4B, enc4->pin_change_b, CHANGE);
 }
 
 void loop() {
