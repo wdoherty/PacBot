7a526c85d44bdfffe652a1472e9f796c2d0a2e76
wdoherty <billydoherty5104@gmail.com>
2019-11-10 16:38:18 -0600

    merged work

---
+++ PacBot2020/include/pins.h
@@ -0,0 +1,8 @@
+#define ENC2A A0
+#define ENC2B A1
+#define ENC3A A2
+#define ENC3B A3
+#define ENC4A A4
+#define ENC4B A5
+#define ENC1A 5
+#define ENC1B 18
--- PacBot2020/src/main.cpp
+++ PacBot2020/src/main.cpp
@@ -1,44 +1,37 @@
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
-
-#define ENC1 36
-#define ENC2 39
+#include <Encoder.h>
+#include "pins.h"
 
 Adafruit_PWMServoDriver pwm;
 
-void updatePin1()
-{
-  Serial.println("Pin1");
-}
-
-void updatePin2()
-{
-  Serial.println("Pin2");
-}
-
 void setup() {
-  // put your setup code here, to run once:
   pwm = Adafruit_PWMServoDriver(0x48);
   Serial.begin(9600);
   pwm.begin();
   pwm.setPWMFreq(500);  // This is the maximum PWM frequency
-  pinMode(ENC1, INPUT);
-  pinMode(ENC2, INPUT);
+  pinMode(ENC1A, INPUT);
+  pinMode(ENC1B, INPUT);
+  pinMode(ENC2A, INPUT);
+  pinMode(ENC2B, INPUT);
+  pinMode(ENC3A, INPUT);
+  pinMode(ENC3B, INPUT);
+  pinMode(ENC4A, INPUT);
+  pinMode(ENC4B, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(ENC1), updatePin1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC2), updatePin2, CHANGE);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while(1)
-  {
-    pwm.setPWM(0, 0, 2048);
-    pwm.setPWM(1, 0, 0);
-    delay(2000);
-    pwm.setPWM(0, 0, 0);
-    delay(2000);
-  }
+  // while(1)
+  // {
+  //   pwm.setPWM(0, 0, 2048);
+  //   pwm.setPWM(1, 0, 0);
+  //   delay(2000);
+  //   pwm.setPWM(0, 0, 0);
+  //   delay(2000);
+  // }
 }
 
 
